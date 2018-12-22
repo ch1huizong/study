e8bb951cd24b478f3e802e98264d43bada4771df
root <root@k.k.lab>
2018-12-22 19:13:01 +0800

    rfc 20多线程部分完成

--- pylearn/rfc/20/coroutins/micro_thread.py
+++ pylearn/rfc/20/coroutins/micro_thread.py
@@ -1,34 +1,38 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:UTF-8 -*-
 # 使用协程完成多线程的任务
 
-def foo():
-    for n in xrange(5):
-        print"I'm foo %d"%n
-        yield
-
-def bar():
-    for n in xrange(10):
-        print"I'm bar %d"%n
-        yield
-
-def spam():
-    for n in xrange(7):
-        print"I'm spam %d"%n
-        yield
 
 from collections import deque
+
+
+def foo():
+    for n in range(5):
+        print("I'm foo %d" % n)
+        yield
+
+
+def bar():
+    for n in range(10):
+        print("I'm bar %d" % n)
+        yield
+
+
+def spam():
+    for n in range(7):
+        print("I'm spam %d" % n)
+        yield
+
+
 taskqueue = deque()
 taskqueue.append(foo())
 taskqueue.append(bar())
 taskqueue.append(spam())
 
 while taskqueue:  # 任务调度
-    #获得下一个任务
     task = taskqueue.pop()
     try:
-        #运行它直到下一条yield和enqueue语句
-        next(task)
+        task.__next__()
         taskqueue.appendleft(task)
     except StopIteration:
         pass
